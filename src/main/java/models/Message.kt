3393153c899cbb49650fa3f75ff7e723a0d98de7
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2020-02-15 23:06:52 +0100

    Resource plain text

--- src/main/java/models/Message.kt
+++ src/main/java/models/Message.kt
@@ -3,7 +3,7 @@ package models
 import java.util.*
 import javax.xml.bind.annotation.XmlRootElement
 
-@XmlRootElement
+@XmlRootElement(name = "Message", namespace = "Message")
 public class Message (
         private var text: String,
         private var id: Long,
