869b1f4cc1ca2817c587ecfceb7ff6f81fbf94de
abdusalamapps <abdusalam.yab1996@gmail.com>
2020-02-15 22:17:28 +0100

    returning an xml response error: Internal Server Error!

--- src/main/java/MessageResource.java
+++ src/main/java/MessageResource.java
@@ -1,14 +1,20 @@
+import models.Message;
+import services.MessageService;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/messages")
 public class MessageResource {
 
+    MessageService messageService = new MessageService();
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMessages() {
-        return "All Messages";
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Message> getMessages() {
+        return messageService.getMessages();
     }
 }
